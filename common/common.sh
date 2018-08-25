191ac51183a48e04a07b2703476f6087ced6ca83
jcc <changejian@gmail.com>
2018-08-25 11:39:03 +0800

    Optimize for coding

--- common/common.sh
+++ common/common.sh
@@ -6,5 +6,5 @@ source ${COMMON_DIR}/ansi.sh
 source ${COMMON_DIR}/spinner.sh
 
 export LOG_PATH=/var/log/laravel-ubuntu-init.log
-export WWW_USER="www-data"
-export WWW_USER_GROUP="www-data"
+export WWW_USER="coding"
+export WWW_USER_GROUP="coding"
